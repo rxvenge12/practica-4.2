42333028ec2e07ad4180bdc45e84dd44fa8c9d0c
Everlasting Despair <vlad.andrejkovets@gmail.com>
2024-05-23 00:32:34 +0300

    Добавьте файлы проекта.

---
+++ practica 4.2/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace practica_4._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.tripleButton = new System.Windows.Forms.Button();
+            this.charTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox1 = new System.Windows.Forms.TextBox();
+            this.resultTextBox2 = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(3, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(818, 22);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // tripleButton
+            // 
+            this.tripleButton.Location = new System.Drawing.Point(3, 68);
+            this.tripleButton.Name = "tripleButton";
+            this.tripleButton.Size = new System.Drawing.Size(416, 36);
+            this.tripleButton.TabIndex = 1;
+            this.tripleButton.Text = "Запуск устроения";
+            this.tripleButton.UseVisualStyleBackColor = true;
+            this.tripleButton.Click += new System.EventHandler(this.tripleButton_Click);
+            // 
+            // charTextBox
+            // 
+            this.charTextBox.Location = new System.Drawing.Point(3, 40);
+            this.charTextBox.Name = "charTextBox";
+            this.charTextBox.Size = new System.Drawing.Size(818, 22);
+            this.charTextBox.TabIndex = 2;
+            // 
+            // resultTextBox1
+            // 
+            this.resultTextBox1.Location = new System.Drawing.Point(3, 110);
+            this.resultTextBox1.Name = "resultTextBox1";
+            this.resultTextBox1.Size = new System.Drawing.Size(818, 22);
+            this.resultTextBox1.TabIndex = 3;
+            // 
+            // resultTextBox2
+            // 
+            this.resultTextBox2.Location = new System.Drawing.Point(3, 138);
+            this.resultTextBox2.Name = "resultTextBox2";
+            this.resultTextBox2.Size = new System.Drawing.Size(818, 22);
+            this.resultTextBox2.TabIndex = 4;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(425, 68);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(396, 36);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Очистка результатов";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 165);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultTextBox2);
+            this.Controls.Add(this.resultTextBox1);
+            this.Controls.Add(this.charTextBox);
+            this.Controls.Add(this.tripleButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button tripleButton;
+        private System.Windows.Forms.TextBox charTextBox;
+        private System.Windows.Forms.TextBox resultTextBox1;
+        private System.Windows.Forms.TextBox resultTextBox2;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ practica 4.2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_4._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void tripleButton_Click(object sender, EventArgs e)
+        {
+            string inputString = inputTextBox.Text;
+            string characterInput = charTextBox.Text;
+
+            // Проверяем, что введен символ
+            if (!string.IsNullOrEmpty(characterInput) && characterInput.Length == 1)
+            {
+                char characterToTriple = characterInput[0];
+
+                // Вариант 1: Используя Replace
+                string result1 = TripleOccurrences(inputString, characterToTriple);
+                resultTextBox1.Text = result1;
+
+                // Вариант 2: Используя StringBuilder
+                string result2 = TripleOccurrencesStringBuilder(inputString, characterToTriple);
+                resultTextBox2.Text = result2;
+            }
+            else
+            {
+                MessageBox.Show("Введите один символ для утроения.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private string TripleOccurrences(string input, char character)
+        {
+            // Заменяем каждое вхождение символа C на три его копии
+            return input.Replace(character.ToString(), new string(character, 3));
+        }
+
+        private string TripleOccurrencesStringBuilder(string input, char character)
+        {
+            // Используем StringBuilder для изменения строки
+            StringBuilder result = new StringBuilder(input);
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] == character)
+                {
+                    result.Insert(i + 1, new string(character, 2)); // Вставляем две копии символа после текущего вхождения
+                    i += 2; // Пропускаем вставленные символы
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
